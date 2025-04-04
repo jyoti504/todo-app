a35b1661b23ede5eff21b8b156eec4445ffe04b0
jyoti <jpal7592@gmail.com>
2025-04-05 00:29:20 +0530

    update  some code

--- script.js
+++ script.js
@@ -16,13 +16,42 @@ function deleteTask(index) {
     updateList();
 }
 
+function deleteTask(index) {
+    todo.splice(index, 1);
+    updateList();
+}
+
+function editNewButton(index) {
+    let newTask = prompt("Edit your task:", todo[index]);
+    if (newTask !== null && newTask.trim() !== "") {
+        todo[index] = newTask.trim();
+        updateList();
+    }
+}
+
 function updateList() {
     let list = document.getElementById("todoList");
     list.innerHTML = "";
 
     for (let i = 0; i < todo.length; i++) {
         let li = document.createElement("li");
-        li.innerHTML = `${todo[i]} <button onclick="deleteTask(${i})">Delete</button>`;
+
+        let editButton = document.createElement("button");
+        editButton.textContent = "Edit";
+        editButton.onclick = function () {
+            editNewButton(i);
+        };
+
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.onclick = function () {
+            deleteTask(i);
+        };
+
+        li.textContent = todo[i];
+        li.appendChild(editButton);
+        li.appendChild(deleteButton);
+
         list.appendChild(li);
     }
 }
